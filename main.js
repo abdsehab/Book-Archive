8971e654e0782dadc07ce59565b98dbd1c0192b9
Sehab-sketch <abdullahalsehab2017@gmail.com>
2021-09-02 12:47:25 +0600

    Show Result Number

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Book Search
+
 const SearchBook = () => {
     const SearchInput = document.getElementById('search-input');
     const SearchInputValue = SearchInput.value;
@@ -12,32 +14,51 @@ const SearchBook = () => {
 
     fetch(API)
         .then(res => res.json())
-        .then(data => ShowResult(data.docs));
+        .then(data => ShowResult(data));
     }
       
 }
+//  Result Part
 
-const ShowResult = Results => {
-    
+const ShowResult = Data => {
+    const Results = Data.docs;
     const ResultBox = document.getElementById('Result-box');
-    
     ResultBox.innerHTML = "";
+    
+    // Result Number
+    const ResulltNumber = document.getElementById('result-found');
+    ResulltNumber.innerHTML = ` Total Result Found : ${Data.num_found}`
+
     Results.forEach(Result => {
         
         const div = document.createElement('div');
         div.classList.add('col');
-        div.innerHTML = `
+
+       
+        
+             div.innerHTML = `
             <div class="card h-100">
                         <img src="https://covers.openlibrary.org/b/id/${Result.cover_i}-M.jpg" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">${Result.title}</h5>
                                 <p class="card-text fw-bold">Author Name : ${Result.author_name} </p>
                                 <p class="card-text ">First Publish Date : ${Result.first_publish_year} </p>
+                                <p class="card-text ">Publisher : ${Result.publisher} </p>
                             </div>
                             
                     </div>
         `;
 
         ResultBox.appendChild(div);
-    });
+        }
+       
+    );
 }
+
+
+
+
+
+
+
+
